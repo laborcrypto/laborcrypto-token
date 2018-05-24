7ab98a13fe9f173825513a05778832fda23c0788
laborcrypto <tkhribech@gmail.com>
2018-05-24 12:47:13 +0530

    ERC-20 Smart Contract For LaborCrypto Token

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# LaborCrypto Token
+
+## Requirements
+	
+Truffle v4.1.8 (core: 4.1.8)
+
+Solidity v0.4.23 (solc-js)
+
+## Need to change (in truffle.js)
+Infura Key for deployment
+Mnemonics
+
+## Steps to deploy
+- npm install
+- truffle compile
+- truffle migrate --network development (for ganache-cli)
+- truffle migrate --network ropsten (for ropsten network)
---
+++ truffle.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+const infura_apikey = "XXXXXX"; // Please Enter Your Own Infura Api key
+const mnemonic = "Enter your own mnemonic from metamask or any of your wallet";
+
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      // gas: 0xfffffffffff,
+      gas: 6712390,
+      network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"+infura_apikey),
+      network_id: 3,
+      gas: 4600000
+    },
+    mainnet: {
+      provider: new HDWalletProvider(mnemonic, "https://mainnet.infura.io/"+infura_apikey),
+      network_id: 3,
+      gas: 4600000
+    }
+  }
+};
